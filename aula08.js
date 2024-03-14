6de1d8f558f10e017fbd1c5a6ccf97da67403264
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-03-14 13:02:54 -0300

    create aula-08

---
+++ aula08.js
@@ -0,0 +1,6 @@
+const salario = '103' ;
+const salarioNum = Number(salario);
+
+console.log('salario:', salario);
+console.log('salario:', salarioNum);
+console.log('salario numero:', salarioNum.toFixed(2));
