2b9bc0931de499b7c09f213bb771d3d6b5a9af14
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-03-14 11:48:37 -0300

    create aula-03

---
+++ aula03.js
@@ -0,0 +1,13 @@
+const idade = 8;
+const nome = "Manetta";
+let podeEntrar = false;
+
+if (idade === "8" && nome == "Manetta"){
+    podeEntrar = true;
+    console.log("Bem vindo!!")
+
+} else{
+    console.log("Não entra!!")
+}
+
+console.log('usuario pode entrar', podeEntrar)
