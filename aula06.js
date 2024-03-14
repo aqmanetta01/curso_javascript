15dd04ae2de6b9198960d41397af07be0db1c49e
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-03-14 12:06:31 -0300

    create aula-05-07

---
+++ aula06.js
@@ -0,0 +1,6 @@
+let idade = 1;
+
+do {
+    console.log('PASSOU', idade);
+    idade++;
+} while(idade < 18);
---
+++ aula07.js
@@ -0,0 +1,12 @@
+const idadeJulio = 18;
+const idadeJose = 14;
+const idadeJoao = 53;
+
+const idades = [18,14,53,14,53,14,53,14,53,14,53, 'string'];
+
+console.log('resultado', idades.length);
+
+for(let i = 0; i<idades.length;i++){
+    console.log('resultado', idades[i]);
+}
+
