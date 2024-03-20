44c1a13c885ca0604ecd949a01b5d4f257c83d74
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-03-20 08:10:05 -0300

    create aula-16

---
+++ aula16.js
@@ -0,0 +1,12 @@
+const numero = 4324;
+const stringA = "string";
+const verdadeOuFalso = true; //false
+
+const arrayTest = [432, 6436, 643];
+
+stringA[4] = 'Julio';
+
+arrayTest.push(473985);
+
+console.log(arrayTest);
+console.log(stringA);
