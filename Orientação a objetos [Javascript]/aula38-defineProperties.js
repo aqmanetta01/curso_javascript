8b494e475820c272af12bf850564eff791b51065
Antônio Manetta <antonio.manetta@watermelontecnologia.com>
2024-04-11 10:08:22 -0300

    create aula38

---
+++ Orientação a objetos [Javascript]/aula38-defineProperties.js
@@ -0,0 +1,26 @@
+const propriedadePadrao = (valorInicial) => ({
+    value: valorInicial,
+    writable: false, // se true => usuario.nome = 'novo nome'
+    configurable: false, // se true => posso deletar o nome
+    enumerable:true  // se true => será exibido no objeto
+            
+})
+
+class Usuario {
+    senha;
+    email;
+
+    constructor(email,senha,nome) {
+        Object.defineProperties(this,{
+            nome: propriedadePadrao(nome),
+            senha: propriedadePadrao(senha),
+            email: propriedadePadrao(email)
+        });
+    }  
+}
+
+const usuario = new Usuario('emails', 'senhas', 'nomes');
+usuario.nome = 'novo nome';
+
+console.log(usuario);
+
